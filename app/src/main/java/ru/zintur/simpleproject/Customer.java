0aee96220cd36ddc32670b3e0a066732f97aedb9
KonstantinFedotovskih <dev@zintur.ru>
2016-04-15 21:59:52 +0500

    Add Customer POJO

--- app/src/main/java/ru/zintur/simpleproject/Customer.java
+++ app/src/main/java/ru/zintur/simpleproject/Customer.java
@@ -5,10 +5,12 @@ public class Customer {
     int id;
     String title;
     String address;
+    String region;
 
-    public Customer(int id, String title, String address) {
+    public Customer(int id, String title, String address, String region) {
         this.id = id;
         this.title = title;
         this.address = address;
+        this.region = region;
     }
 }
